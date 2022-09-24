0527157f425540c6f87130ffac7344094a153889
bfarabi <bfarab108@gmail.com>
2022-09-24 22:48:22 +0600

    pull requ

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Slider from 'react-slick';
+import Slider from '../Slider/Slider';
 import About from '../About/About';
 import Contact from '../contact/Contact';
 import Footer from '../footer/Footer';
